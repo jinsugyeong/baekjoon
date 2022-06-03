1c52ccc884813fe46638b0dcabda212dcfbe2843
jinsugyeong <apr15th@naver.com>
2022-06-03 11:38:51 +0900

    백준 1단계 사칙연산

---
+++ src/step/one/Calculation.java
@@ -0,0 +1,33 @@
+package step.one;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Calculation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(a+b));
+        bw.newLine();
+        bw.write(String.valueOf(a-b));
+        bw.newLine();
+        bw.write(String.valueOf(a*b));
+        bw.newLine();
+        bw.write(String.valueOf(a/b));
+        bw.newLine();
+        bw.write(String.valueOf(a%b));
+        bw.flush();
+        bw.close();
+    }
+    
+}
