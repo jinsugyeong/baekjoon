c1241405c78d43ca3fc3d17cc8d7f7c12639cb5b
jinsugyeong <apr15th@naver.com>
2022-06-03 11:48:01 +0900

    백준 1단계 10926번 ??!

---
+++ src/step/one/Iouse.java
@@ -0,0 +1,22 @@
+package step.one;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Iouse {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        str += "??!";
+
+        bw.write(str);
+        bw.flush();
+        bw.close();
+
+    }
+}
