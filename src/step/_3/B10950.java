e2aa669a8554d46158a7689787b0b7d599676e99
jinsugyeong <apr15th@naver.com>
2022-06-07 12:53:12 +0900

    백준 3단계 10950번 A+B-3

---
+++ src/step/_3/B10950.java
@@ -0,0 +1,32 @@
+package step._3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B10950 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int count = Integer.parseInt(br.readLine());
+        int sum[] = new int[count];
+        int a,b;
+
+        for(int i=0; i<count; i++){
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            sum[i] = a+b;
+        }
+
+        for(int  j: sum){
+            System.out.println(j);
+        }
+    }
+}
