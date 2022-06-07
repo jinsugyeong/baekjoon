6edac3b884208d0e3036d3aa170fb984026c0d33
jinsugyeong <apr15th@naver.com>
2022-06-07 14:17:46 +0900

    백준 3단계 11022번 A+B -8

---
+++ src/step/_3/B11022.java
@@ -0,0 +1,22 @@
+package step._3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        StringTokenizer st; int a,b;
+
+        for(int i=1; i<=cnt; i++){
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            System.out.println("Case #"+ i +": "+a + " + " + b + " = " + (a+b));
+        }
+    }
+}
