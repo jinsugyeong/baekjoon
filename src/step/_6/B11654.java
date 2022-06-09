3a16c6ae4348906be7deefd8f663fa129cb83451
jinsugyeong <apr15th@naver.com>
2022-06-09 18:16:34 +0900

    백준 6단계 11654번 아스키 코드

---
+++ src/step/_6/B11654.java
@@ -0,0 +1,14 @@
+package step._6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = br.readLine().charAt(0);
+
+        System.out.println(num);
+    }
+}
