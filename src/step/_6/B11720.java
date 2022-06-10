f4869506bf8a53bd7f4ead3f6ed0692b1dac60e5
jinsugyeong <apr15th@naver.com>
2022-06-10 09:43:24 +0900

    백준 6단계 11720번 숫자의 합

---
+++ src/step/_6/B11720.java
@@ -0,0 +1,24 @@
+package step._6;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        int arr[] = new int[size];
+        int sum = 0;
+
+        String input = br.readLine();
+        String[] inputArr = input.split("");
+        for(int i=0; i<size; i++){
+            arr[i] = Integer.parseInt(inputArr[i]);
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+    }
+}
