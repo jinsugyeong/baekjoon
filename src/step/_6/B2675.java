3409741b08ad0c6a5789a0f9b1a31b220a508b5c
jinsugyeong <apr15th@naver.com>
2022-06-13 09:08:21 +0900

    백준 6단계 2675번 문자열반복(다시)

--- src/step/_6/B2675.java
+++ src/step/_6/B2675.java
@@ -1,32 +1,39 @@
 package step._6;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class B2675 {
  
-	public static void main(String[] args) throws IOException {
- 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-	
-			String[] str = br.readLine().split(" ");	// 공백 분리
-			
-			int R = Integer.parseInt(str[0]);	// String -> int
-			String S = str[1];
-			
-			for(int j = 0; j < S.length(); j++) {
-				for(int k = 0; k < R; k++) {
-					System.out.print(S.charAt(j));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int cnt = Integer.parseInt(br.readLine());
+
+
+		for(int i=0; i<cnt; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int forCnt = Integer.parseInt(st.nextToken());
+			String forStr = st.nextToken();
+			char[] forStrArr = forStr.toCharArray();
+
+			for(char c : forStrArr){
+				for(int j=0; j<forCnt; j++){
+					bw.write(c);
 				}
 			}
-			System.out.println();
+			bw.write("\n");
+
 		}
-	}
+
+		bw.flush();
+		bw.close();
  
- 
+	} 
 }
