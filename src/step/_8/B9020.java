84859f8b6985962d35de9ce40bc22b7295c78d78
jinsugyeong <apr15th@naver.com>
2022-06-21 16:44:59 +0900

    백준 8단계 9020번 골드바흐의 추측  / 참고 블로그 : https://st-lab.tistory.com/91  / boolean 배열을 이용하여 10000이내의 소수를 구해놓고 입력한 짝수 n의 1/2 값에서 소수가 될 때까지 1씩 증가,감소

--- src/step/_8/B9020.java
+++ src/step/_8/B9020.java
@@ -6,67 +6,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-/**
- * 첫 번째 수를 짝수n의 1/2값으로 설정한 뒤 
- * for문으로 소수가 아닐 시 1씩 마이너스시키고 
- * 두 번째 수를 짝수n에서 첫 번째 수를 뺀 값으로 설정하고
- * 두 번째 수가 소수가 아니면 다시 첫 번째 수를 재설정하게 하고 싶어서
- * 재귀호출을 썼는데 계속 오류가나서 실패
- */
-
 public class B9020 {
-    public static int solution(int num, int a, int b){
 
-        for(int i=2; i <= Math.sqrt(b); i++){
-            if(b % i == 0){
-                a = b - 1;
-            }
-        }
-
-        if(a <= 0){
-            a = b;
-        }else {
-            b = num - a;
-        }
-
-        boolean isPrime = true;
-        for(int i=2; i <= Math.sqrt(b); i++){
-            if(b % i == 0){
-                isPrime = false;
-                break;
-                // System.out.println("a: "+ a + " b: "+ b);
-            }
-        }        
-
-        if(isPrime){
-            return b;
-        }else {
-            return solution(num, a, b);
-        }
-    }
-
+    public static boolean[] prime = new boolean[10001];
     public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        get_prime();
+
         int cnt = Integer.parseInt(br.readLine());
 
-        while(cnt != 0){
-
+        while(cnt--> 0){
             int num = Integer.parseInt(br.readLine());
-            int b = num/2;
-            int a = 0;
+            int p = num / 2;
+            int q = num / 2;
 
-            b = solution(num, a, b);
-            a = num - b;
-
-            System.out.println(a);
-            System.out.println(b);
-
-            cnt--;
+            while(true){
+                if(!prime[p] && !prime[q]) {
+                    bw.write(p+" "+q+"\n");
+                    break;
+                }
+                p--;
+                q++;
+            }
         }
 
         bw.flush();
         bw.close();
+    }
+
+    static void get_prime(){
+        prime[0] = prime[1] = true;
+
+        for(int i=2; i<=Math.sqrt(prime.length); i++){
+            if(prime[i]) continue;
+            
+            for (int j=i*i; j<prime.length; j += i){
+                prime[j] = true;
+            }
+        }
     }
 }
