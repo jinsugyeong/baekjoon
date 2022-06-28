95914462c8f18c0226d2ba6847fe372f88a01cc6
jinsugyeong <apr15th@naver.com>
2022-06-28 10:31:46 +0900

    백준 11단계 2751번 수 정렬하기 2

---
+++ src/step/_11/B2751.java
@@ -0,0 +1,33 @@
+package step._11;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class B2751 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+        Set<Integer> s = new TreeSet<>();
+
+        for(int i=0; i<cnt; i++){
+            s.add(Integer.parseInt(br.readLine()));
+        }
+
+        Iterator<Integer> iter = s.iterator();
+        while(iter.hasNext()){
+            bw.write(iter.next() + "\n");
+        }
+        bw.flush();
+        bw.close();
+
+        
+    }
+}
