6a82610f4a399f1edec33ca216c28785b91ba450
jinsugyeong <apr15th@naver.com>
2022-06-30 15:08:45 +0900

    백준 11단계 11650번 좌표정렬하기 / 참고블로그 : https://st-lab.tistory.com/110

--- src/step/_11/B11650.java
+++ src/step/_11/B11650.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B11650 {
@@ -14,26 +15,20 @@ public class B11650 {
 
         int N = Integer.parseInt(br.readLine());
         int arr[][] = new int[N][2];
-        
-        int tmp[] = new int[2];
 
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        
-
-        for(int i=0; i<N-1; i++){
-            for(int j=i+1; j<N; j++){
-                if(arr[i][0] > arr[j][0] || (arr[i][0] == arr[j][0] && arr[i][1] > arr[j][1])){
-                    tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                }
+        //sort 메서드 람다식 이용
+        Arrays.sort(arr, (a, b) -> {
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            }else {
+                return a[0] - b[0];
             }
-        }
+        });
 
         for(int i=0; i<N; i++){
             bw.write(arr[i][0] + " " + arr[i][1]+"\n");
