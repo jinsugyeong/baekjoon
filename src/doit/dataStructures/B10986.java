fb3b8e67e583b97fd360a26b20241a5a9474d7a0
jinsugyeong <apr15th@naver.com>
2023-05-16 20:18:28 +0900

    구간 합 - 10986번 나머지 합 : 책 정답코드 보고

--- src/doit/dataStructures/B10986.java
+++ src/doit/dataStructures/B10986.java
@@ -4,40 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class B10986 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int arr[] = new int[N];
-        int sumArr[] = new int[N];
-        int cnt[] = new int[M];
-        int result = 0;
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long[] S = new long[N];
+        long[] C = new long[M];
+        long result = 0;
+        S[0] = sc.nextInt();
 
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-
-            if(i > 0) sumArr[i] = sumArr[i-1] + arr[i];
-            else sumArr[i] = arr[i];
-
-            sumArr[i] %= M;
-            cnt[sumArr[i]]++;
+        for(int i=1; i<N; i++) {
+            S[i] = S[i-1] + sc.nextInt();
         }
 
-        result += cnt[0];
+        for(int i=0; i<N; i++) {
+            int remainder = (int) (S[i] % M);
 
-        for(int i=0; i<M; i++){
-            if(cnt[i] > 1){
-                result += (cnt[i] * (cnt[i] - 1) / 2);
-            }
+            if (remainder == 0) result ++;
+            C[remainder]++;
+        }
+
+        for(int i=0; i<M; i++) {
+            if(C[i] > 1) result += (C[i] * (C[i] - 1) / 2);
         }
 
         System.out.println(result);
     }
+
 }
